a976c00d0d9c5f1fb01240fbe183227b44b142d0
LeChosenOne <aceofblades51@gmail.com>
2016-02-14 22:32:44 -0500

    Initial Commit

---
+++ Codiggity/CoDiggity.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace CoDiggity
+{
+    partial class Codiggity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Codiggity));
+            this.tTheme = new System.Windows.Forms.TextBox();
+            this.bGo = new System.Windows.Forms.Button();
+            this.audio = new AxWMPLib.AxWindowsMediaPlayer();
+            this.tOutput = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.audio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tTheme
+            // 
+            this.tTheme.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tTheme.Location = new System.Drawing.Point(190, 161);
+            this.tTheme.Name = "tTheme";
+            this.tTheme.Size = new System.Drawing.Size(100, 35);
+            this.tTheme.TabIndex = 0;
+            // 
+            // bGo
+            // 
+            this.bGo.Location = new System.Drawing.Point(12, 202);
+            this.bGo.Name = "bGo";
+            this.bGo.Size = new System.Drawing.Size(172, 35);
+            this.bGo.TabIndex = 1;
+            this.bGo.Text = "Commence Rap";
+            this.bGo.UseVisualStyleBackColor = true;
+            this.bGo.Click += new System.EventHandler(this.bGo_Click);
+            // 
+            // audio
+            // 
+            this.audio.Enabled = true;
+            this.audio.Location = new System.Drawing.Point(12, 44);
+            this.audio.Name = "audio";
+            this.audio.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("audio.OcxState")));
+            this.audio.Size = new System.Drawing.Size(10, 12);
+            this.audio.TabIndex = 2;
+            this.audio.Visible = false;
+            // 
+            // tOutput
+            // 
+            this.tOutput.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tOutput.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tOutput.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tOutput.ForeColor = System.Drawing.Color.White;
+            this.tOutput.Location = new System.Drawing.Point(0, 473);
+            this.tOutput.Multiline = true;
+            this.tOutput.Name = "tOutput";
+            this.tOutput.ReadOnly = true;
+            this.tOutput.Size = new System.Drawing.Size(486, 356);
+            this.tOutput.TabIndex = 3;
+            this.tOutput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(486, 162);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(262, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 34);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Presenting...";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(364, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Save Rap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(190, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Save Lyrics";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.progressBar3);
+            this.groupBox1.Controls.Add(this.progressBar2);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 145);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 294);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(83, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 24);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "British";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(297, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Demo?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.ForeColor = System.Drawing.Color.Red;
+            this.progressBar3.Location = new System.Drawing.Point(6, 149);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(450, 20);
+            this.progressBar3.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar3.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.progressBar2.ForeColor = System.Drawing.Color.Yellow;
+            this.progressBar2.Location = new System.Drawing.Point(6, 123);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(450, 20);
+            this.progressBar2.TabIndex = 0;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.progressBar1.Location = new System.Drawing.Point(6, 97);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(450, 20);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // CodeDiggity
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(486, 829);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tOutput);
+            this.Controls.Add(this.audio);
+            this.Controls.Add(this.bGo);
+            this.Controls.Add(this.tTheme);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "CodeDiggity";
+            this.Text = "CodeDiggity";
+            this.Load += new System.EventHandler(this.CodeDiggity_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.audio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tTheme;
+        private System.Windows.Forms.Button bGo;
+        private AxWMPLib.AxWindowsMediaPlayer audio;
+        private System.Windows.Forms.TextBox tOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Codiggity/CoDiggity.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+using System.Media;
+using System.Threading;
+using System.Diagnostics;
+
+namespace CoDiggity
+{
+    public partial class Codiggity : Form
+    {
+        List<String> nouns = File.ReadAllLines("lib/nouns.txt").ToList<String>();
+        List<String> verbs = File.ReadAllLines("lib/verbs.txt").ToList<String>();
+        List<String> adjectives = File.ReadAllLines("lib/adjectives.txt").ToList<String>();
+        List<String> adverbs = File.ReadAllLines("lib/adverbs.txt").ToList<String>();
+        List<String> prepositions = File.ReadAllLines("lib/prepositions.txt").ToList<String>();
+        List<String> pronouns = File.ReadAllLines("lib/pronouns.txt").ToList<String>();
+        string block = "";
+        bool wipe = false;
+
+        Random rand = new Random();
+
+        public Codiggity()
+        {
+            InitializeComponent();
+        }
+        private string getRhyme(string word)
+        {
+            string url = "http://rhymebrain.com/talk?function=getRhymes&word=" + word; //+ word
+            var proxy = WebRequest.Create(url);
+            var response = proxy.GetResponse();
+            var stream = response.GetResponseStream();
+
+            StreamReader sReader = new StreamReader(stream);
+
+            string json = sReader.ReadToEnd();
+
+            Word[] items = JsonConvert.DeserializeObject<Word[]>(json);
+            int length = items.Count();
+            int randomWord = rand.Next(1, length);
+            int i = 0;
+            while (items[randomWord].score < 300 && i < 1000)
+            {
+                randomWord = rand.Next(1, length);
+                i++;
+            }
+            return items[randomWord].word;
+        }
+        private string getPartOfSpeach(string word)
+        {
+            string url = "http://dictionary.reference.com/browse/" + word + "?s=t";
+            var proxy = WebRequest.Create(url);
+            var response = proxy.GetResponse();
+            var stream = response.GetResponseStream();
+            StreamReader sReader = new StreamReader(stream);
+            string json = sReader.ReadToEnd();
+            string partOfSpeach = Regex.Split(((Regex.Split(json, "class=\"dbox-pg\">")[1])), "</span>")[0];
+            return partOfSpeach;
+
+        }
+        private string findSynonym(string word)
+        {
+            string url = "http://www.thesaurus.com/browse/" + word + "?s=t";
+            var proxy = WebRequest.Create(url);
+            var response = proxy.GetResponse();
+            var stream = response.GetResponseStream();
+            StreamReader sReader = new StreamReader(stream);
+            string json = sReader.ReadToEnd();
+            string partOfSpeach = Regex.Split(((Regex.Split(json, "<div class=\"relevancy-list\" style=\"margin-left: 0px;\">")[1])), "<span class=\"text\">")[1];
+            //			string partOfSpeach = Regex.Split(Regex.Split (((Regex.Split (json, "<div class=\"relevancy-list\" style=\"margin-left: 0px;\">") [1])), "<span class=\"text\">") [1], "</span>")[0];
+            return partOfSpeach;
+        }
+        private string generateSentence(string rhymeWord)
+        {
+            int nounRand = rand.Next(nouns.Count() - 1);
+            int nounRand2 = rand.Next(nouns.Count() - 1);
+            int verbRand = rand.Next(verbs.Count() - 1);
+            int adjectiveRand = rand.Next(adjectives.Count() - 1);
+            int adverbRand = rand.Next(adverbs.Count() - 1);
+            int prepRand = rand.Next(prepositions.Count() - 1);
+            int pronounRand = rand.Next(pronouns.Count() - 1);
+
+            string noun = nouns.ToArray()[nounRand];
+            string adjective = "";
+            string article = "a";
+            if (adjectiveRand % 4 >=  1)
+            {
+                adjective = adjectives.ToArray()[adjectiveRand];
+                char first = adjective.ToCharArray()[0];
+                if (first.Equals('a') || first.Equals('e') || first.Equals('i') || first.Equals('o') || first.Equals('u'))
+                {
+                    article += 'n';
+                }
+            }
+            else
+            {
+                char first = noun.ToCharArray()[0];
+                if (first.Equals('a') || first.Equals('e') || first.Equals('i') || first.Equals('o') || first.Equals('u'))
+                {
+                    article += 'n';
+                }
+            }
+            if (pronounRand % 2 == 0)
+            {
+                article = "the";
+            }
+            string preposition = "";
+            if (nounRand % 2 == 0)
+            {
+                preposition = prepositions.ToArray()[prepRand] + " ";
+            }
+            string subject = "";
+            if (rand.Next(1, 10) < 8)
+            {
+                subject = pronouns.ToArray()[pronounRand];
+            }
+            return (subject + " " + verbs.ToArray()[verbRand] + " " + preposition
+                + article + " " + adjectives.ToArray()[adjectiveRand] + " " + rhymeWord);
+        }
+        private void bGo_Click(object sender, EventArgs e)
+        {
+            tOutput.Clear();
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            if (checkBox1.Checked)
+            {
+                synth.SelectVoice("Microsoft Server Speech Text to Speech Voice (en-GB, Hazel)");
+            }
+            else
+            {
+                synth.SelectVoice("Microsoft Server Speech Text to Speech Voice (en-US, Helen)");
+            }
+            if (block.StartsWith("Let me show"))
+            {
+                tTheme.Text = "demo";
+                goto speak;
+            }
+            /*String voices = "";
+            foreach(InstalledVoice v in synth.GetInstalledVoices())
+            {
+                voices += v.VoiceInfo.Name + "\n";
+            }
+            MessageBox.Show(voices);
+            //			SpeechSynthesizer synth = new SpeechSynthesizer();
+            //			synth.SelectVoice("Microsoft Zira Desktop");
+            //			synth.Volume = 100;
+            //			synth.Rate = 1; http://www.dictionaryapi.com/api/v1/references/collegiate/xml/tTheme.Text?key=9307aec8-5ca3-476c-8a1b-0100a646c429*/
+            string rhymeWord = getRhyme(tTheme.Text);
+            while (getPartOfSpeach(rhymeWord) != "noun")
+            {
+                rhymeWord = getRhyme(tTheme.Text);
+            }
+            int nounRand = rand.Next(nouns.Count() - 1);
+            string noun = nouns.ToArray()[nounRand];
+            //string syn1 = findSyn(noun)[(new Random).Next(findSyn(noun).Count())];
+            string firstParagraph = generateSentence(tTheme.Text) + Environment.NewLine + generateSentence(getRhyme(tTheme.Text)) + Environment.NewLine + generateSentence(noun)  + Environment.NewLine + generateSentence(getRhyme(noun));
+            nounRand = rand.Next(nouns.Count() - 1);
+            noun = nouns.ToArray()[nounRand];
+            nounRand = rand.Next(nouns.Count() - 1);
+            string noun1 = nouns.ToArray()[nounRand];
+            string secondParagraph = "\n\r\n\r" + generateSentence(noun1) + Environment.NewLine + generateSentence(getRhyme(noun1)) + Environment.NewLine + generateSentence(noun) +   Environment.NewLine + generateSentence(getRhyme(noun));
+            nounRand = rand.Next(nouns.Count() - 1);
+            noun = nouns.ToArray()[nounRand];
+            nounRand = rand.Next(nouns.Count() - 1);
+            noun1 = nouns.ToArray()[nounRand];
+            string thirdParagraph = "\n\r\n\r" + generateSentence(noun1) + Environment.NewLine + generateSentence(getRhyme(noun1)) + Environment.NewLine + generateSentence(noun) + Environment.NewLine +  generateSentence(getRhyme(noun));
+            nounRand = rand.Next(nouns.Count() - 1);
+            noun = nouns.ToArray()[nounRand];
+            string finalTwo = "\n\r\n\r" + generateSentence(noun) + Environment.NewLine + generateSentence(getRhyme(noun));
+        //MessageBox.Show(firstParagraph + secondParagraph + thirdParagraph + finalTwo);
+            synth.Rate = 1;
+            //tOutput.AppendText(synth.Voice.Name + Environment.NewLine);
+            block = firstParagraph + secondParagraph + thirdParagraph + finalTwo;
+            speak:
+            synth.SetOutputToWaveFile("lib\\" + tTheme.Text + ".wav");
+            tOutput.AppendText(block);
+            synth.Speak(block);
+
+            SoundPlayer simpleSound = new SoundPlayer(Directory.GetCurrentDirectory() + "\\lib\\" + tTheme.Text + ".wav");
+
+            synth.SetOutputToDefaultAudioDevice();
+            string[] startPhrase = { "giddy up", "buckle up", "wiggity wiggity wack", "are you ready?", "listen up" };
+            int selectPhase = rand.Next(0,startPhrase.Count());
+            synth.Speak(startPhrase[selectPhase]);
+            Thread.Sleep(200);
+
+            string[] songs = {"SecondSample.m4a", "ThirdSampleMP4.m4a", "FourthSampleMP4.m4a" };
+            int selectSong = rand.Next(0, songs.Count());
+            var p2 = new System.Windows.Media.MediaPlayer();
+            string backround = Directory.GetCurrentDirectory() + "\\lib\\" + songs[selectSong];
+            p2.Volume = 0.2;
+            p2.Open(new System.Uri(backround));
+            DateTime first = DateTime.Now;
+            p2.Play();
+            progressBar1.Step = 1;
+            progressBar1.Value = 0;
+            while ((DateTime.Now - first).Seconds < 6.5)
+            {
+                Random gen = new Random();
+                progressBar1.Value = gen.Next(10,30);
+                progressBar2.Value = gen.Next(20,40);
+            }
+            simpleSound.Play();
+            first = DateTime.Now;
+            while((DateTime.Now-first).Seconds < 34)
+            {
+                Random gen = new Random();
+                progressBar1.Value = gen.Next(20,40);
+                progressBar2.Value = gen.Next(30,60);
+                progressBar3.Value = gen.Next(60, 100);
+
+            }
+            p2.Volume = 0.1;
+            while ((DateTime.Now - first).Seconds < 2)
+            {
+                Random gen = new Random();
+                progressBar1.Value = gen.Next(20, 40);
+                progressBar2.Value = gen.Next(30, 60);
+                progressBar3.Value = gen.Next(60, 100);
+
+            }
+            if (wipe)
+            {
+                block = "";
+                wipe = false;
+            }
+            first = DateTime.Now;
+            simpleSound.Stop();
+            p2.Stop();
+            button1.Enabled = true;
+            button2.Enabled = true;
+            progressBar1.Value = 0;
+            progressBar2.Value = 0;
+            progressBar3.Value = 0;
+            /*
+            var p1 = new System.Windows.Media.MediaPlayer();
+            string speech = Directory.GetCurrentDirectory() + "\\lib\\" + tTheme.Text + ".wav";
+            MessageBox.Show(speech);
+            p1.Volume = 100;
+            p1.Open(new System.Uri(speech));
+            p1.Play();*/
+
+        }
+
+        private void CodeDiggity_Load(object sender, EventArgs e)
+        {
+           // this.WindowState = FormWindowState.Maximized;
+        }
+
+        private string[] findSyn(string word)
+        {
+            string url = "http://www.thesaurus.com/browse/" + word + "?s=t";
+            var proxy = WebRequest.Create(url);
+            var response = proxy.GetResponse();
+            var stream = response.GetResponseStream();
+            StreamReader sReader = new StreamReader(stream);
+            string json = sReader.ReadToEnd();
+            string[] partOfSpeach = Regex.Split(json, "<span class=\"text\">");
+            string[] synonyms = new string[partOfSpeach.Count() - 1];
+            for (int i = 1; i < partOfSpeach.Count(); i++)
+            {
+                string synonym = Regex.Split(partOfSpeach[i], "</span>")[0];
+                synonyms[i - 1] = synonym;
+            }
+            return synonyms;
+        }
+
+        private void logo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rap saved to " + Directory.GetCurrentDirectory() + "\\lib\\" + tTheme.Text + ".wav");
+            openFileDialog1.InitialDirectory = Directory.GetCurrentDirectory() + "\\lib\\";
+            openFileDialog1.ShowDialog();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(Directory.GetCurrentDirectory() + "\\lib\\" + tTheme.Text + ".txt", block);
+            Process.Start("notepad.exe", Directory.GetCurrentDirectory() + "\\lib\\" + tTheme.Text + ".txt");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            block = "Let me show you how we do it in Rutgers son " + Environment.NewLine +
+                "You'll be back to hello world by the time we are done " + Environment.NewLine +
+                "Codiggity is fire, our program is so fresh" + Environment.NewLine +
+                "But don't look at our code, it's a bit of a mess" + Environment.NewLine +
+                "We're here at hack B U, with a rapping machine" + Environment.NewLine +
+                "Let's get on with the demo, there's so much to be seen.";
+            wipe = true;
+            bGo.PerformClick();
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    public class Word
+    {
+        public string word { get; set; }
+        public string freq { get; set; }
+        public int score { get; set; }
+        public string flags { get; set; }
+        public string syllables { get; set; }
+    }
+
+}
